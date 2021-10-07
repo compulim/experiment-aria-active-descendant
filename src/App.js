1550fef3688e084d57d5650a56b17c4b23511c26
William Wong <compulim@users.noreply.github.com>
2021-10-07 21:48:41 +0000

    Cosmetics

--- src/App.js
+++ src/App.js
@@ -19,16 +19,25 @@ function App() {
       </p>
       <h1>Steps</h1>
       <ol>
-        <li>Start screen reader</li>
-        <li>On Windows: Disable scan mode by pressing <kbd>CAPS</kbd> + <kbd>SPACE</kbd> (higher pitch sound in NVDA indicate scan mode is disabled)</li>
+        <li>Start screen reader.</li>
         <li>
-          Press <kbd>TAB</kbd> to focus to the list below
+          Press <kbd>TAB</kbd> to focus to the list below, should see a solid black border.
         </li>
         <li>
-          Press <kbd>UP</kbd>/<kbd>DOWN</kbd> arrow keys to navigate across items.
+          Disable scan mode by pressing <kbd>CAPS</kbd> + <kbd>SPACE</kbd> (higher pitch sound in NVDA indicate scan
+          mode is disabled).
+          <ul>
+            <li>Apple VoiceOver do not have scan mode.</li>
+          </ul>
         </li>
-        <li>EXPECT: Screen reader should read each of the list item correctly.</li>
+        <li>
+          Press <kbd>UP</kbd>/<kbd>DOWN</kbd> arrow keys to navigate across items, should see both solid and dashed
+          border.
+        </li>
       </ol>
+      <p>
+        <strong>EXPECT</strong>: When using arrow keys to navigate, screen reader should read each of the list item correctly.
+      </p>
       <h1>Try this out</h1>
       <ActiveDescendant ref={activeDescendantRef} />
       <h1>Live View of HTML</h1>
@@ -38,23 +47,31 @@ function App() {
         <li>
           Edge + Windows Narrator
           <ul>
-            <li>✔️ Do narrate content on arrow keys.</li>
-            <li>❌ Do not narrate "Return to the Hundred Acre Wood". Seems like a browser bug.</li>
+            <li>✔️ Do narrate content on arrow keys (when active descendant change).</li>
+            <li>
+              ❌ Do not narrate "Return to the Hundred Acre Wood". It narrate "An authorised sequel was published on
+              ...". Seems like a browser bug.
+            </li>
           </ul>
         </li>
         <li>
           Chrome + NVDA:
           <ul>
-            <li>❌ Do not narrate content on arrow keys.</li>
-            <li>❌ Do not narrate "Return to the Hundred Acre Wood". Seems like a browser bug.</li>
+            <li>❌ Do not narrate content on arrow keys (when active descendant change), only narrate "1 of 5".</li>
+            <li>
+              ❌ Do not narrate "Return to the Hundred Acre Wood". It narrate "An authorised sequel was published on
+              ...". Seems like a browser bug.
+            </li>
           </ul>
         </li>
         <li>
           Firefox + NVDA:
           <ul>
-            <li>✔️ Do narrate content on arrow keys.</li>
-            <li>✔️ Do narrate "Return to the Hundred Acre Wood". Seems like a browser bug.</li>
-            <li>❌ Do not narrate "link" (accessible name) for "Return to the Hundred Acre Wood".</li>
+            <li>✔️ Do narrate content on arrow keys (when active descendant change).</li>
+            <li>✔️ Do narrate "Return to the Hundred Acre Wood".</li>
+            <li>
+              ❌ Do not narrate "link" (accessible name) for "Return to the Hundred Acre Wood", treated as plain text.
+            </li>
           </ul>
         </li>
         <li>
@@ -62,7 +79,8 @@ function App() {
           <ul>
             <li>✔️ Do narrate SOME content on arrow keys: "An authorised sequel and 2 more items."</li>
             <li>
-              ✔️ Do narrate "link" (accessible name) when pressing <kbd>CAPS</kbd> + <kbd>RIGHT</kbd>: "link, Return to the Hundred Acre Wood".
+              ✔️ Do narrate "link" (accessible name) when pressing <kbd>CAPS</kbd> + <kbd>RIGHT</kbd>: "link, Return to
+              the Hundred Acre Wood".
             </li>
           </ul>
         </li>
